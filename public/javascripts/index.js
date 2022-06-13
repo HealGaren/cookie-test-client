f53c539c7af8083287b988a7d6ca6ccd9b39f3b1
최예찬 <cyc1007@midasin.com>
2022-06-13 21:00:07 +0900

    withCredentials true 처리

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,4 +2,4 @@ const queryString = new URLSearchParams(location.search);
 const apiDomain = queryString.get('apiDomain') ?? window.location.host;
 const cookieDomain = queryString.get('cookieDomain') ?? window.location.host;
 
-axios.get(`https://${apiDomain}/api/environments?domain=${cookieDomain}`);
+axios.get(`https://${apiDomain}/api/environments`, {params: {domain: cookieDomain}}, {withCredentials: true});
